deb2860a62ed7ffe6374af8c3f6d7265b7a0abf5
welison0 <welisoncarlos123@gmail.com>
2025-09-04 19:44:30 +0000

    figuras adicionadas no tabuleiro, cone,cruz e octaedro.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,33 +4,29 @@ int main() {
     char linha[10] = {'A','B','C','D','E','F','G','H','I','J'};
     int tabuleiro[10][10];
 
-    // 1. Inicializar tabuleiro com 0 (água)
+    // 1. Inicializar tabuleiro com 0
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
             tabuleiro[i][j] = 0;
         }
     }
 
-    //navio 1- linha 3, coluna e,f,g.
-    for(int j = 4; j <= 6; j++){
-        tabuleiro[2][j] = 3;
-    }
+    // ===== Cone (1) no topo (coluna central = 4) =====
+    tabuleiro[0][4] = 1; 
+    tabuleiro[1][3] = 1; tabuleiro[1][4] = 1; tabuleiro[1][5] = 1;
+    tabuleiro[2][2] = 1; tabuleiro[2][3] = 1; tabuleiro[2][4] = 1; tabuleiro[2][5] = 1; tabuleiro[2][6] = 1;
 
-    //navio 2- coluna H, linha 6,7,8.
-    for(int i = 5; i <= 7; i++){
-        tabuleiro[i][7] = 3;
-    }
-     //navio 3, diagonal
-    for(int k = 0; k < 4; k++){
-        tabuleiro[4+k][3+k] = 3;
-    }
+    // ===== Cruz (2) na parte de baixo à esquerda =====
+    // Centro da cruz em (7,2)
+    tabuleiro[7][2] = 2; 
+    tabuleiro[6][2] = 2; tabuleiro[8][2] = 2; 
+    tabuleiro[7][0] = 2; tabuleiro[7][1] = 2; tabuleiro[7][3] = 2; tabuleiro[7][4] = 2;
 
-    //navio 4, diagonal
-    for(int k = 0; k < 3; k++){
-        tabuleiro[1+k][1+k] = 3;
-    }
-     
-    printf("\n");
+    // ===== Octaedro (3) na parte de baixo à direita =====
+    // Centro em (7,7)
+    tabuleiro[6][7] = 3;
+    tabuleiro[7][6] = 3; tabuleiro[7][7] = 3; tabuleiro[7][8] = 3;
+    tabuleiro[8][7] = 3;
 
     // 2. Exibir tabuleiro
     printf(" TABULEIRO BATALHA NAVAL \n");
@@ -42,12 +38,12 @@ int main() {
 
     for(int i = 0; i < 10; i++){
        if(i+1 < 10)
-       printf(" %d ", i+1);
+           printf(" %d ", i+1);
        else
-       printf("%d", i+1);
+           printf("%d", i+1);
 
        for(int j = 0; j < 10; j++){
-        printf("%d ", tabuleiro[i][j]);
+           printf("%d ", tabuleiro[i][j]);
        }
        printf("\n");
     }
